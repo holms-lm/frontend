71db6e661b5028c926d74b5b031b77b53cf20e02
holms <serg8822446@gmail.com>
2019-04-04 13:38:13 +0300

    1338

--- homeworks/19_03_19 media mobile/gulpfile.js
+++ homeworks/19_03_19 media mobile/gulpfile.js
@@ -1,12 +1,17 @@
 'use strict';
- 
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
- 
+var postcss = require('gulp-postcss');
+var nano = require('cssnano');
+var autoprefixer = require('autoprefixer');
+// var autopref = require('gulp-autoprefixer')
+
 sass.compiler = require('node-sass');
- 
+// .pipe(autopref({browsers:['last 2 version']}))
 gulp.task('scss', function () {
   return gulp.src('./styles/scss/flex_style.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./styles/'));
+      .pipe(sass().on('error', sass.logError))
+      .pipe(postcss([autoprefixer({browsers: ['last 5 versions']}), nano()]))
+      .pipe(gulp.dest('./styles/'));
 });
