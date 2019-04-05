61a2a0e58664594e1173a15673bd67b878485d04
holms <serg882244@gmail.com>
2019-04-05 22:05:59 +0300

    homework js test if()else

---
+++ homeworks/test of Java script/quastion_prompt.js
@@ -0,0 +1,9 @@
+function quastion() {
+    var name_js = prompt('Какаво "официальное" название JavaScript?','JavaScript');
+    if(name_js == 'EcmaScript'){
+        alert('Верно!');
+    }
+    else {
+        alert('Не знаете? "EcmaScript!"');
+    }
+}
